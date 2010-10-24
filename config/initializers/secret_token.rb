f3c034f349c412596a727623df99cc576e360eee
Rudiger Lippert <rudy.lippert@originatelabs.com>
2010-10-24 12:31:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rudash::Application.config.secret_token = 'd15e2fd35994b390903c3fb1d000e3ff2bf87ff2521a6c5c1da1b274d92838d5e6d2c5bbd9f787bf4cfc33b92da39299dd7009469f1cad005c4b497c6a2d504b'
