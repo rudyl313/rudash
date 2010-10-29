78554f3381107d01bea7aba2d029110380fb8094
Rudiger Lippert <rudy.lippert@originatelabs.com>
2010-10-29 00:27:50 -0700

    Got basic seed file ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create(:login => "temp",:email => "temp@email.com", :password => "temp", :password_confirmation => "temp", :users_permission => "yes")
