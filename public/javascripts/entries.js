9e40a95bd30acb6aae2f884b89e3802aeea35a04
Rudiger Lippert <rudiger@rudiger-BN405AV-ABA-HPE-190t.(none)>
2010-11-21 17:08:22 -0800

    Rearchitected layout to make jquery code easier to implement

--- public/javascripts/entries.js
+++ public/javascripts/entries.js
@@ -1,33 +1,35 @@
 (function($) {
   $(function(){
+
     $(".date_container").click(function(){
-      // Figure out how to properly move a div
-      $("#add_entry_controls").hide();
-      $(".entry_controls",$(this).parent()).append($("#add_entry_controls"));
-      $("#add_entry_controls").show();
+      $(".entry_controls").slideUp();
+      $(".entry_controls",$(this).parent()).slideDown();
     });
-    $("#submit_new_entry").click(function(e){
+
+    $(".submit_new_entry").click(function(e){
       e.preventDefault();
-      var $group = $(this).parent().parent().parent();
+      var $group = $(this).parent().parent();
+      var datestr = $group.attr("data-date");
       $.ajax({
         url : $(this).attr("data-path"),
         type : 'POST',
         data : {
           entry : {
-            due_time : $("#entry_time_field").val(),
-            due_date : $group.attr("data-date"),
-            content : $("#entry_content_field").val()
+            due_time : $("#time_field_" + datestr).val(),
+            due_date : datestr,
+            content : $("#content_field_" + datestr).val()
           }
         },
         success : function(data,x,y){
-          $("#add_entry_controls").slideUp();
+          $(".entry_controls").slideUp();
           $(".entry_list",$group).slideUp();
           $(".entry_list",$group).html(data);
           $(".entry_list",$group).slideDown();
-          $("#entry_content_field").val("");
-          $("#entry_time_field").val("");
+          $("#time_field_" + datestr).val("");
+          $("#content_field_" + datestr).val("");
         }
       });
     });
+
   });
 })(jQuery);
