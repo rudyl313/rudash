36002b9445d88a1cea187bfa593e3c9198435807
Rudiger Lippert <rudiger@rudiger-BN405AV-ABA-HPE-190t.(none)>
2010-11-22 23:18:30 -0800

    Completed basic drag and drop functionality for switching dates

--- public/javascripts/entries.js
+++ public/javascripts/entries.js
@@ -33,17 +33,18 @@
 
     $(".entry").draggable({
       revert: 'invalid',
-      //revert : true,
       refreshPositions: true
-      //helper : 'clone',
     });
 
     $(".date_group").droppable({
       drop : function(e,ui){
         var dropped_date = $(this).attr("data-date");
-        var dragged_date = $(ui.draggable).parent().parent().attr("data-date");
-        var dragged_id = $(ui.draggable).attr("data-id");
-        $(".entry_list",$(this)).slideUp();
+        var $dragged_item = $(ui.draggable);
+        var $dragged_group = $dragged_item.parent().parent();
+        var dragged_date = $dragged_group.attr("data-date");
+        var dragged_id = $dragged_item.attr("data-id");
+        var $dropped_group = $(".entry_list",$(this));
+        $dropped_group.slideUp();
         $(".entry_list",$("[data-date='" + dragged_date  + "']")).slideUp();
         $.ajax({
           url : $(ui.draggable).attr("data-update-path"),
@@ -54,7 +55,14 @@
             }
           },
           success : function(data,x,y){
-            alert("ajax worked");
+            $dropped_group.html(data);
+            $dropped_group.slideDown();
+            $(".entry",$dropped_group).draggable({
+              revert: 'invalid',
+              refreshPositions: true
+            });
+            $dragged_item.remove();
+            $(".entry_list",$dragged_group).slideDown();
           }
         });
       }
