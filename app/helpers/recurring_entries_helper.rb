12dfab9274d26ea5cf483ad12956506ee4c54ba2
Rudiger Lippert <rudiger@rudiger-BN405AV-ABA-HPE-190t.(none)>
2010-11-29 20:56:20 -0800

    Made fields more specific in recurring events

--- app/helpers/recurring_entries_helper.rb
+++ app/helpers/recurring_entries_helper.rb
@@ -1,2 +1,14 @@
 module RecurringEntriesHelper
+  def day_options
+    (0..6).to_a.map { |i| [week_day(i),i] }
+  end
+
+  def month_options
+    (1..12).to_a.map { |i| [month_string(i),i] }
+  end
+
+  def month_string(num)
+    arr = [nil,"January","February","March","April","May","June","July","August","September","October","November","December"]
+    arr[num]
+  end
 end
